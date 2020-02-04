c411d8a1b9f8a2ec467a6fc9aa843d8a077095b2
Vladyslav Pekker <agilesteel@gmail.com>
2020-02-04 15:02:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Installs [Homebrew](https://docs.brew.sh/Homebrew-on-Linux) on [Ubuntu](https://www.ubuntu.com/)
+
+```bash
+wget -q https://script.install.devinsideyou.com/homebrew
+sudo chmod +x homebrew && ./homebrew
+```
---
+++ install-homebrew.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# remove yourself
+rm $0
+
+# install homebrew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+# add it to path
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+echo
+echo '"homebrew" is now on the path'
